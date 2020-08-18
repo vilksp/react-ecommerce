a812ef761eb68444c653847dd9f2a16ce002c5bf
vilksp <v.kasperavicius1@gmail.com>
2020-08-19 01:28:52 +0300

    collection items added, header added with links,routes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import React from "react";
 import "./App.css";
 import HomePage from "./pages/homepage/home.component";
-import { Route } from "react-router-dom";
-
-const HatsPage = () => (
-  <div>
-    <h1>Hats Page</h1>
-  </div>
-);
+import { Route, Switch } from "react-router-dom";
+import ShopPage from "./pages/shop/shop.component.jsx";
+import Header from "./component/header/header.component";
 
 function App() {
   return (
     <div>
-      <Route exact path="/" component={HomePage} />
-      <Route path="/hats" component={HatsPage} />
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+      </Switch>
     </div>
   );
 }
